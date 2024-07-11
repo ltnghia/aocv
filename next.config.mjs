f7c2962127ff7dcad52b90ec71a7d8ad5076cd28
ldnam153 <duynamvt79@gmail.com>
2024-07-11 17:44:20 +0700

    loader

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     output: "export",
     basePath: "/lava-workshop",
     reactStrictMode: true,
+    images:{
+        loader:"custom",
+        loaderFile:"src/custom-image-loader.js",
+    }
+    
 };
 
 export default nextConfig;
