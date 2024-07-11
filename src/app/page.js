51f9916ebff6c1539837864f3ebd71d9b777af21
ldnam153 <duynamvt79@gmail.com>
2024-07-11 17:21:49 +0700

    header changes

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,7 @@ export default function Home() {
   useEffect(()=>{
     window.onscroll = function() {scrollFunction()};
     mybutton = document.getElementById("backToTop");
+    mybutton.style.display = "none";
   })
 
   function scrollFunction() {
