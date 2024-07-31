7678280bbe604ef3292cdaaacb2b4715a516cbf9
vominhduc <vmduc.khtn@gmail.com>
2024-07-31 16:30:21 +0900

    Update challenge.jsx

--- src/app/components/challenge.jsx
+++ src/app/components/challenge.jsx
@@ -10,20 +10,24 @@ const Challenge = () => {
             </p> */}
 
             <p>
-            <span className="font-bold">Challenge Overview:</span> The primary goal of this challenge is to advance the capability of Large Vision-Language Models to accurately interpret and understand complex visual data such as Data Flow Diagrams (DFDs), Class Diagrams, Gantt Charts, and Building Design Drawings. We invite AI researchers, data scientists, and practitioners with interest and experience in natural language processing, computer vision, and multimodal learning to join this workshop challenge. Participants can register as individuals or teams. They are required to develop a model that can answer questions related to the input data. Below, we provide a few samples. Our challenge is conducted in two separate phases.
+            <span className="font-bold">Challenge Overview:</span> The primary goal of this challenge is to advance the capability of Large Vision-Language Models to accurately interpret and understand complex visual data such as Data Flow Diagrams (DFDs), Class Diagrams, Gantt Charts, and Building Design Drawings. We invite AI researchers, data scientists, and practitioners with interest and experience in natural language processing, computer vision, and multimodal learning to join this workshop challenge. Participants can register as individuals or teams. They are required to develop a model that can answer questions related to the input data. Below, we provide a few samples.
 
             <br/>
             <span className="font-bold">Datasets:</span>
             <ul class="list-disc list-inside space-y-2 mt-2 ml-4">
-                <li><span className="font-bold">Phase 1:</span> We will build the dataset by collecting data from the internet. Note that we will only release the test set. Participants can freely use publicly available datasets to develop their models.</li>
-                <li><span className="font-bold">Phase 2:</span> We will release a test set (without answers). Additionally, participants are required to submit their reproducible implementations so that we can test them on our private dataset provided by the TASUKI team (SoftBank).</li>
+                <li><span className="font-bold">Public dataset with answers:</span> We will build the dataset by collecting data from the internet. Participants can freely use publicly available datasets to develop their models.</li>
+                <li><span className="font-bold">Public dataset without answers:</span> We will release the dataset without answers. Participants will submit the results to our system to get the scores.</li>
+                <li><span className="font-bold">Private dataset:</span> Participants are required to submit their reproducible implementations so that we can test them on our private dataset provided by the TASUKI team (SoftBank).</li>
             </ul>
+
+            <br/>
+            Please register your team information <a href='https://docs.google.com/forms/d/e/1FAIpQLScvuNuXqoHGW3SKhF2WYN34WhFarVp3Dx6gEXMX4dr4xT7T6A/viewform'>here</a>. We will send the link to download the dataset later.
             
             <span className="font-bold">Metric:</span>
             <br/>
             We will evaluate using MMMU metric. 
             
-            Final score = 0.3 * Phase 1 result + 0.5 * Phase 2 (public set) result + 0.2 * Phase 2 (private set) result.
+            Final score = 0.3 * Public dataset with answers + 0.5 * Public dataset without answers + 0.2 * Private dataset.
 
             <br/>
             <span className="font-bold">Baselines</span> (coming soon)
