1d2b9bf35adc77df249e67f5785a4f2358773847
Huy H. Nguyen <honghuy127@gmail.com>
2024-08-05 13:13:10 +0900

    Update organizers.jsx

--- src/app/components/organizers.jsx
+++ src/app/components/organizers.jsx
@@ -4,11 +4,11 @@ const Organizers = ()=>{
     return <div className="text-justify"> 
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4">
-            <PersonAvatar name="Duc Minh Vo" image="/avatar.jpg" affiliation={"University of Tokyo, Japan"} />
+            <PersonAvatar name="Duc Minh Vo" image="/vmduc.jpg" affiliation={"University of Tokyo, Japan"} />
             <PersonAvatar name="Huy H. Nguyen" image="/nhhuy.jpg" affiliation={"National Institute of Informatics, Japan"} />
             <PersonAvatar name="Trung-Nghia Le" image="/ltnghia.jpg" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Akihiro Sugimoto" image="/sugimoto.jpg" affiliation={"National Institute of Informatics, Japan"} />
-            <PersonAvatar name="Hideki Nakayama" image="/avatar.jpg" affiliation={"University of Tokyo, Japan"} />
+            <PersonAvatar name="Hideki Nakayama" image="/nakayama.jpg" affiliation={"University of Tokyo, Japan"} />
             <PersonAvatar name="Minh-Triet Tran" image="/tmtriet.png" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Khan Md Anwarus Salam" image="/avatar.jpg" affiliation={"SoftBank, Japan"} />
          
