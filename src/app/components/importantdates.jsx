616c63ce7a9cbfe7ea737199ea8cb34a23253a56
Huy H. Nguyen <honghuy127@gmail.com>
2024-08-05 13:19:35 +0900

    Update importantdates.jsx

--- src/app/components/importantdates.jsx
+++ src/app/components/importantdates.jsx
@@ -2,7 +2,7 @@ const ImportantDates = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>Challenge track opened: <span className="font-bold">2024/7/31</span></li>
+            <li>Challenge track opened: <span className="font-bold">2024/8/15</span></li>
             <li>Test set released:  <span className="font-bold">2024/8/30</span></li>
             <li>Challenge track closed: <span className="font-bold">2024/9/30</span></li>
             <li>Regular paper submission deadline: <span className="font-bold">2024/9/30</span></li>
