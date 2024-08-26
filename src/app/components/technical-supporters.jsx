42b370f12538102b43bce5fcce55e9135821c33f
vominhduc <vmduc.khtn@gmail.com>
2024-08-26 14:05:32 +0900

    Update technical-supporters.jsx

--- src/app/components/technical-supporters.jsx
+++ src/app/components/technical-supporters.jsx
@@ -6,6 +6,7 @@ const TechnicalSupporters = ()=>{
         <div className="grid sm:grid-cols-3 grid-cols-2 mt-4">
             <PersonAvatar name="Duy-Nam Ly" image="/ldnam.jpg" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Trong-Le Do" image="/dtle.png" affiliation={"University of Science, Vietnam"} />
+            <PersonAvatar name="Daichi Sato" image="/Daichi.jpg" affiliation={"The University of Tokyo, Japan"} />
             <PersonAvatar name="TASUKI Team" image="/TASUKI.png" affiliation={"SoftBank, Japan"} />
         </div>
 
