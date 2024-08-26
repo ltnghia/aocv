80442a5682e3a72d9cdc4f1afffa19c443e1b9f6
vominhduc <vmduc.khtn@gmail.com>
2024-08-26 14:08:14 +0900

    Update technical-supporters.jsx

--- src/app/components/technical-supporters.jsx
+++ src/app/components/technical-supporters.jsx
@@ -3,7 +3,7 @@ import PersonAvatar from "./person-avatar";
 const TechnicalSupporters = ()=>{
     return <div className="text-justify"> 
          
-        <div className="grid sm:grid-cols-3 grid-cols-2 mt-4">
+        <div className="grid sm:grid-cols-4 grid-cols-2 mt-4">
             <PersonAvatar name="Duy-Nam Ly" image="/ldnam.jpg" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Trong-Le Do" image="/dtle.png" affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Daichi Sato" image="/Daichi.jpg" affiliation={"The University of Tokyo, Japan"} />
