690e743eff00d8ab3caa7af472352b28a58a8dbc
vominhduc <vmduc.khtn@gmail.com>
2024-12-04 14:46:20 +0900

    Update speakers.jsx

--- src/app/components/speakers.jsx
+++ src/app/components/speakers.jsx
@@ -6,12 +6,18 @@ const Speakers = ()=>{
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4">
             <PersonAvatar name="Asim Munawar" image="/AsimMunawar.jpg" affiliation={"IBM Research, US"} />
             <p>
-            LLM-Based Reasoning: Opportunities and Pitfalls <br/>
+            <b>LLM-Based Reasoning: Opportunities and Pitfalls </b>
+            <br/>
             The question of whether Large Language Models (LLMs) can reason is both complex and crucial. While LLMs have demonstrated capabilities in informal reasoning, their potential extends far beyond, particularly in advancing explainable AI and the development of intelligent agents. In this talk, I will explore the reasoning abilities of LLMs and discuss the ongoing efforts at IBM to enhance these capabilities in IBM granite models by using knowledge and tool-assisted synthetic data generation.
 
             </p>
             
-{/*             <PersonAvatar name="April Pyone Maung Maung" image="/Maung.jpg" affiliation={"National Institute of Informatics, Japan"} /> */}
+            <PersonAvatar name="April Pyone Maung Maung" image="/Maung.jpg" affiliation={"National Institute of Informatics, Japan"} />
+            <p>
+            <b>Adversarial Attacks and Defenses on Vision-Language Models </b>
+            <br/>
+            Robustness research aims to build machine learning models that are resilient to unpredictable accidents (black swans and tail risk robustness) and carefully crafted and deceptive threats (adversarial robustness). In this talk, I shall focus on the adversarial robustness of vision-language models (VLMs). Specifically, I shall cover recent attacks and defenses on two kinds of VLMs: vision-language pre-trained models like CLIP and vision-conditioned LLMs like LLaVA. The talk will focus on discussing whether existing attacks are practical and what the challenges and opportunities of adversarial machine learning research are in the space of vision-language models.
+            </p>
         </div>
 
     </div>
