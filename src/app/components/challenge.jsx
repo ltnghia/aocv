dbcd3410b36d4f3735ccb78c618a43cbed7e752d
vominhduc <vmduc.khtn@gmail.com>
2024-12-11 16:01:57 +0900

    Update challenge.jsx

--- src/app/components/challenge.jsx
+++ src/app/components/challenge.jsx
@@ -27,12 +27,12 @@ const Challenge = () => {
                     <li><span className="font-bold">Public dataset:</span> We will release our dataset collected from the internet. It contains about 3,000 samples. </li>
                     <li><span className="font-bold">Private dataset:</span> The TASUKI team (SoftBank) provides this dataset. It contains about 1,100 samples.</li>
                 </ul>
-                <div className="mt-2">The data contain English and Japanese texts. <span className="font-bold"><a href=' https://drive.google.com/drive/folders/1YBH2FyYfRpppI-hhJTSVgqNt_uBQjY6E' className="underline text-[1.4em] text-[#232ba1]">You can download the data here</a></span>. Please carefully read the <a href='https://docs.google.com/document/d/1VvKlzi6KfpaYuN_YuhQGzphRuXyJlOM_/edit?usp=sharing&ouid=103136956899952170060&rtpof=true&sd=true' className="underline text-[1.4em] text-[#232ba1]">Terms and Conditions</a> for further information about the license, data, and submission instructions.</div>
+{/*                 <div className="mt-2">The data contain English and Japanese texts. <span className="font-bold"><a href=' https://drive.google.com/drive/folders/1YBH2FyYfRpppI-hhJTSVgqNt_uBQjY6E' className="underline text-[1.4em] text-[#232ba1]">You can download the data here</a></span>. Please carefully read the <a href='https://docs.google.com/document/d/1VvKlzi6KfpaYuN_YuhQGzphRuXyJlOM_/edit?usp=sharing&ouid=103136956899952170060&rtpof=true&sd=true' className="underline text-[1.4em] text-[#232ba1]">Terms and Conditions</a> for further information about the license, data, and submission instructions.</div>
                 <div className="mt-2">Participants are required to submit the results to our system to get the scores.</div>
                 <br />
                 <br />
                 <span className="font-bold"><a href='https://docs.google.com/forms/d/e/1FAIpQLScvuNuXqoHGW3SKhF2WYN34WhFarVp3Dx6gEXMX4dr4xT7T6A/viewform' className="underline text-[1.4em] text-[#232ba1]">Register your team information here</a></span>. We will send the link to download the dataset to registered participants.
-                <br />
+                <br /> */}
                 <br />
 
                 <span className="font-bold">Metric:</span>
@@ -84,11 +84,11 @@ const Challenge = () => {
 
                 <span className="font-bold">Computational Resources:</span>
                 Participants from the University of Tokyo may use SoftBank Beyond AI SANDBOX GPUs.</div>
-                <br/>
+{/*                 <br/>
                 <span className="font-bold "><a href='https://cmt3.research.microsoft.com/LAVA2024' className="underline text-[1.4em] text-[#232ba1]">Submit your report here</a></span>.
                 <br />
                 <span className="font-bold"><a href='https://www.codabench.org/competitions/3587' className="underline text-[1.4em] text-[#232ba1]">Submit your results here </a></span>
-                <br />
+                <br /> */}
 
         </div>
     )
