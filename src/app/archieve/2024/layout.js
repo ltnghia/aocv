2c33aed177669418a842df1ce8e622cf3bfeb970
vominhduc <vmduc.khtn@gmail.com>
2025-03-02 01:06:09 +0900

    Create layout.js

---
+++ src/app/archieve/2024/layout.js
@@ -0,0 +1,21 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "./components/header";
+import Footer from "./components/footer";
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "LAVA-Workshop"
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Header />
+          {children}
+        <Footer />
+        </body>
+    </html>
+  );
+}
