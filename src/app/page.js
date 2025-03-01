d134f4719b71d249c328a0f1c86b6bd78bb82b4b
vominhduc <vmduc.khtn@gmail.com>
2025-03-02 01:56:20 +0900

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -68,33 +68,33 @@ export default function Home() {
           ACCV 2024
         </a>
       </div>
-      // <Section title={""} body={<Workshop />} className={"mt-8"} id="workshop" />
-      // <Section title={"Accepted Papers"} body={<AcceptedPapers />} id="acceptedpapers" />
-      // <Section title={"Call for Papers"} body={<CallForPapers />} id="callforpapers" />
-      // <Section title={"LAVA Challenge"} body={<Challenge />} id="challenge" />
-      // <Section title={"Important Dates"} body={<ImportantDates />} id="importantdates" />
-      // <Section title={"Workshop Schedule"} body={<Schedule />} id="schedule" />
-      // <Section title={"Speakers"} body={<Speakers />} id="speakers" />
-      // <Section title={"Organizers"} body={<Organizers />} id="organizers" />
-      // <Section title={"Technical Supporters"} body={<TechnicalSupporters />} id="TechnicalSupporters" />
-      // <Section title={"Workshop Sponsors"}
-      //   body={
-      //     <div className="flex flex-row w-full justify-center items-center ">
-      //       <div className="flex flex-col justify-center ">
-      //         <Image src="/BeyondAI.jpg" alt="BeyondAI" width={200} height={200} className="sm:w-[18m] w-[12em]" />
-      //       </div>
-      //     </div>
+      <Section title={""} body={<Workshop />} className={"mt-8"} id="workshop" />
+      <Section title={"Accepted Papers"} body={<AcceptedPapers />} id="acceptedpapers" />
+      <Section title={"Call for Papers"} body={<CallForPapers />} id="callforpapers" />
+      <Section title={"LAVA Challenge"} body={<Challenge />} id="challenge" />
+      <Section title={"Important Dates"} body={<ImportantDates />} id="importantdates" />
+      <Section title={"Workshop Schedule"} body={<Schedule />} id="schedule" />
+      <Section title={"Speakers"} body={<Speakers />} id="speakers" />
+      <Section title={"Organizers"} body={<Organizers />} id="organizers" />
+      <Section title={"Technical Supporters"} body={<TechnicalSupporters />} id="TechnicalSupporters" />
+      <Section title={"Workshop Sponsors"}
+        body={
+          <div className="flex flex-row w-full justify-center items-center ">
+            <div className="flex flex-col justify-center ">
+              <Image src="/BeyondAI.jpg" alt="BeyondAI" width={200} height={200} className="sm:w-[18m] w-[12em]" />
+            </div>
+          </div>
 
-      //   }
-      //   id="sponsors" />
-      // <div className="fixed bottom-6 right-6">
-      //   <button id="backToTop" className="bg-[#01305f] hover:bg-[#234880] text-white font-bold py-4 px-4 rounded-full shadow-lg" onClick={() => topClick()}>
-      //     <svg width="20" height="20" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
-      //       <path d="M33.7879 60H26.2121V14.5455L5.37879 35.3788L0 30L30 0L60 30L54.6212 35.3788L33.7879 14.5455V60Z" fill="white" />
-      //     </svg>
+        }
+        id="sponsors" />
+      <div className="fixed bottom-6 right-6">
+        <button id="backToTop" className="bg-[#01305f] hover:bg-[#234880] text-white font-bold py-4 px-4 rounded-full shadow-lg" onClick={() => topClick()}>
+          <svg width="20" height="20" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M33.7879 60H26.2121V14.5455L5.37879 35.3788L0 30L30 0L60 30L54.6212 35.3788L33.7879 14.5455V60Z" fill="white" />
+          </svg>
 
-      //   </button>
-      // </div>
+        </button>
+      </div>
     </main>
   );
 }
