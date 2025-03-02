8a2ae105740b991ba91a471df36feeb97bd692a2
Duy-Nam Ly <51230585+ldnam153@users.noreply.github.com>
2025-03-02 15:18:29 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,17 @@
 /** @type {import('next').NextConfig} */
 import path from 'path';
+// Resolve the current directory using import.meta.url (ES module approach)
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
+// Ensure loaderFile points to the correct relative path
+const loaderFilePath = path.resolve(__dirname, 'src/custom-image-loader.js');
+
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
     images: { 
         unoptimized: true,
         loader:"custom",
-        loaderFile: path.resolve(__dirname, 'src/custom-image-loader.js'),
+        loaderFile: loaderFilePath,
     },
     basePath: '/aocv'
 };
