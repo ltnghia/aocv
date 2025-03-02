d53ec44c467e011b4173774a79412f507251161a
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 23:59:08 +0700

    Update header.jsx

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -5,13 +5,13 @@ const Header = ()=>{
         <div className="header">
             <div className="justify-center flex flex-col sm:text-[2em] text-[1.5em] text-[#f5f5f5]  font-medium ">
                 <div className="text-white text-center text-[1.7em]">
-                     <span className="text-white tracking-[0.35em] font-semibold">LAVA </span>
+                     <span className="text-white tracking-[0.35em] font-semibold">AOCV </span>
                 </div>
                 <div className="text-[0.8em] text-center mt-2">
                     ACM Multimedia 2025 Workshop on  
                 </div>
                 <div className="text-[0.8em] text-center ">
-                    Ambiguous Object Analysis in Computer Vision (AOCV) 
+                    Ambiguous Object Analysis in Computer Vision 
                 </div>
                 <div className="text-[0.55em]  text-white text-center mt-5">
                     Dublin, Ireland 27 - 31.10.2025
