83e82a192c2d1c1210a88b62c7d5438f57987442
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 15:46:06 +0700

    Update footer.jsx

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -5,7 +5,16 @@ const Footer = () => {
         <footer className="h-[10em] ">
             <div className="flex flex-row justify-between w-[70em] px-2">
                 <div className="flex flex-col justify-center">
-                    <Image src={`${imagePrefix}/hcmus-logo-white.png`} alt="HCMUS" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                    <Image src={`${imagePrefix}/hcmus-logo-white.png`} alt="University of Science" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                </div>
+                <div className="flex flex-col justify-center">
+                    <Image src={`${imagePrefix}/hcmus-logo-white.png`} alt="University of Dayton" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                </div>
+                <div className="flex flex-col justify-center">
+                    <Image src={`${imagePrefix}/hcmus-logo-white.png`} alt="University of Information Technology" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                </div>
+                <div className="flex flex-col justify-center">
+                    <Image src={`${imagePrefix}/hcmus-logo-white.png`} alt="Monash University" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
                 </div>
             </div>
 
