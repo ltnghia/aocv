db7bbff0c75b7391d101697343af3ee886417188
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 15:42:49 +0700

    Update technical-supporters.jsx

--- src/app/components/technical-supporters.jsx
+++ src/app/components/technical-supporters.jsx
@@ -5,9 +5,7 @@ const TechnicalSupporters = ()=>{
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4">
             <PersonAvatar name="Duy-Nam Ly" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Trong-Le Do" image={`${imagePrefix}/dtle.png`} affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Daichi Sato" image={`${imagePrefix}/Daichi.jpg`} affiliation={"The University of Tokyo, Japan"} />
-            <PersonAvatar name="TASUKI Team" image={`${imagePrefix}/TASUKI.png`} affiliation={"SoftBank, Japan"} />
+            <PersonAvatar name="Viet-Tham Huynh" image={`${imagePrefix}/dtle.png`} affiliation={"University of Science, Vietnam"} />
         </div>
 
         <br/>
