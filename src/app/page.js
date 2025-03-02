1bb062e525526eb0faf7b0ea26c68c81f0756d00
Duy-Nam Ly <51230585+ldnam153@users.noreply.github.com>
2025-03-03 00:21:49 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -32,7 +32,7 @@ export default function Home() {
         mybutton.style.display = "none";
     }
   }
-  const button = "hover:cursor-pointer text-[#01305f] hover:text-white border border-[#01305f] hover:bg-[#01305f] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-4 mb-2"
+  const button = "hover:cursor-pointer text-[#790819] hover:text-white border border-[#790819] hover:bg-[#790819] focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-4 mb-2"
   return (
     <main className="flex min-h-screen flex-col items-center   sm:px-24 px-4 ">
       <div className="sm:flex flex-row grid mt-10 sm:w-[65em] w-full justify-center">
@@ -65,7 +65,7 @@ export default function Home() {
       <Section title={"Organizers"} body={<Organizers />} id="organizers" />
   {/* <Section title={"Technical Supporters"} body={<TechnicalSupporters />} id="TechnicalSupporters" /> */}
       <div className="fixed bottom-6 right-6">
-        <button id="backToTop" className="bg-[#01305f] hover:bg-[#234880] text-white font-bold py-4 px-4 rounded-full shadow-lg" onClick={() => topClick()}>
+        <button id="backToTop" className="bg-[#790819] hover:bg-[#802323] text-white font-bold py-4 px-4 rounded-full shadow-lg" onClick={() => topClick()}>
           <svg width="20" height="20" viewBox="0 0 60 60" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M33.7879 60H26.2121V14.5455L5.37879 35.3788L0 30L30 0L60 30L54.6212 35.3788L33.7879 14.5455V60Z" fill="white" />
           </svg>
