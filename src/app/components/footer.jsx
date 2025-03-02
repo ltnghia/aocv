2a7acbf89ef4eaaec3f07acf3e57945cb405b986
Duy-Nam Ly <51230585+ldnam153@users.noreply.github.com>
2025-03-02 15:30:28 +0700

    Update footer.jsx

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -5,19 +5,19 @@ const Footer = () => {
         <footer className="h-[10em] ">
             <div className="flex flex-row justify-between w-[70em] px-2">
                 <div className="flex flex-col justify-center">
-                    <Image src=`${basePath}/hcmus-logo-white.png` alt="UTokyo" width={110} height={200} className="sm:w-[6em] w-[4em]" />
+                    <Image src={`${basePath}/hcmus-logo-white.png`} alt="UTokyo" width={110} height={200} className="sm:w-[6em] w-[4em]" />
                 </div>
                 <div className="flex flex-col justify-center ">
-                    <Image src=`${basePath}/hcmus-logo-white.png` alt="NII" width={100} height={50} className="sm:h-[5em] h-[5em]" />
+                    <Image src={`${basePath}/hcmus-logo-white.png`} alt="NII" width={100} height={50} className="sm:h-[5em] h-[5em]" />
                 </div>
                 <div className="flex flex-col justify-center">
-                    <Image src="/hcmus-logo-white.png" alt="footer" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                    <Image src={`${basePath}/hcmus-logo-white.png`} alt="footer" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
                 </div>
                 <div className="flex flex-col justify-center">
-                    <Image src=`${basePath}/hcmus-logo-white.png` alt="Beyond AI" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                    <Image src={`${basePath}/hcmus-logo-white.png`} alt="Beyond AI" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
                 </div>
                 <div className="flex flex-col justify-center">
-                    <Image src=`${basePath}/hcmus-logo-white.png` alt="TASUKI" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
+                    <Image src={`${basePath}/hcmus-logo-white.png`} alt="TASUKI" width={150} height={50} className="sm:w-[7em] w-[4em]"/>
                 </div>
                 {/* <div className="flex flex-col justify-center">
                     <Image src="/softbank.png" alt="Soft Bank" width={200} height={200} className="sm:w-[13em] w-[6em]" />
