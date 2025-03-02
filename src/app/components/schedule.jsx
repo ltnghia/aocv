20a5b7c0cae0fd6b17a482eccbc956fc3f0e6cfe
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 16:50:06 +0700

    Update schedule.jsx

--- src/app/components/schedule.jsx
+++ src/app/components/schedule.jsx
@@ -3,7 +3,7 @@ const Schedule = () => {
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
             <li> <span className="font-bold">13:30</span> - Opening Remark</li>
             <li><span className="font-bold">13:40</span> - Keynote Talk</li>
-            <li><span className="font-bold">14:40</span> - Poster session</li>
+            <li><span className="font-bold">14:40</span> - Paper Presentation</li>
             <li><span className="font-bold">16:55</span> - Closing Remark</li>
         </ul>
     </div>
