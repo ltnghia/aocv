005dfb9ea041e134c2d79610facd9ce25eb20b87
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 16:52:24 +0700

    Update acceptedpapers.jsx

--- src/app/components/acceptedpapers.jsx
+++ src/app/components/acceptedpapers.jsx
@@ -1,7 +1,6 @@
 const AcceptedPapers = () => {
     return (
         <div className="text-justify">
-            <span className="font-bold text-[1.3em]">Workshop track</span>
             <ol className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
 {/*                 <li>Duc-Tuan Luu, Viet-Tuan Le, Duc Minh Vo: Questioning, Answering, and Captioning for Zero-Shot Detailed Image Caption</li> */}
             </ol>
