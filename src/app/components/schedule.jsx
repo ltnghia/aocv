76e37109fd946a788b5c8ba53e055786707be314
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 16:50:39 +0700

    Update schedule.jsx

--- src/app/components/schedule.jsx
+++ src/app/components/schedule.jsx
@@ -1,10 +1,10 @@
 const Schedule = () => {
     return <div className="text-justify">
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li> <span className="font-bold">13:30</span> - Opening Remark</li>
-            <li><span className="font-bold">13:40</span> - Keynote Talk</li>
-            <li><span className="font-bold">14:40</span> - Paper Presentation</li>
-            <li><span className="font-bold">16:55</span> - Closing Remark</li>
+            <li> <span className="font-bold"> </span> - Opening Remark</li>
+            <li><span className="font-bold"> </span> - Keynote Talk</li>
+            <li><span className="font-bold"> </span> - Paper Presentation</li>
+            <li><span className="font-bold"> </span> - Closing Remark</li>
         </ul>
     </div>
 }
