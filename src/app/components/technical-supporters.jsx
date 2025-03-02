4f1cd1c2bea5a804891bde54fee3dd49a562adbb
Duy-Nam Ly <51230585+ldnam153@users.noreply.github.com>
2025-03-02 15:41:34 +0700

    Update technical-supporters.jsx

--- src/app/components/technical-supporters.jsx
+++ src/app/components/technical-supporters.jsx
@@ -1,13 +1,13 @@
 import PersonAvatar from "./person-avatar";
-
+import { imagePrefix } from "../../constant.js";
 const TechnicalSupporters = ()=>{
     return <div className="text-justify"> 
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4">
-            <PersonAvatar name="Duy-Nam Ly" image="/ldnam.jpg" affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Trong-Le Do" image="/dtle.png" affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Daichi Sato" image="/Daichi.jpg" affiliation={"The University of Tokyo, Japan"} />
-            <PersonAvatar name="TASUKI Team" image="/TASUKI.png" affiliation={"SoftBank, Japan"} />
+            <PersonAvatar name="Duy-Nam Ly" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Science, Vietnam"} />
+            <PersonAvatar name="Trong-Le Do" image={`${imagePrefix}/dtle.png`} affiliation={"University of Science, Vietnam"} />
+            <PersonAvatar name="Daichi Sato" image={`${imagePrefix}/Daichi.jpg`} affiliation={"The University of Tokyo, Japan"} />
+            <PersonAvatar name="TASUKI Team" image={`${imagePrefix}/TASUKI.png`} affiliation={"SoftBank, Japan"} />
         </div>
 
         <br/>
