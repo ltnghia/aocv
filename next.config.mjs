b47ad1c74abd4911b18d8cdca6dd0fbb91e0b642
Duy-Nam Ly <51230585+ldnam153@users.noreply.github.com>
2025-03-02 15:12:38 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
     output: "export",
     reactStrictMode: true,
-    images: { unoptimized: true },
+    images: { 
+        unoptimized: true,
+        loader:"custom",
+        loaderFile: path.resolve(__dirname, 'src/custom-image-loader.js'),
+    },
     basePath: '/aocv'
 };
 
