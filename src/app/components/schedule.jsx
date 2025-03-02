5bfd6d2205d2ea0b79c7162d3fa73cdd001734d2
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-02 16:08:04 +0700

    Update schedule.jsx

--- src/app/components/schedule.jsx
+++ src/app/components/schedule.jsx
@@ -2,12 +2,9 @@ const Schedule = () => {
     return <div className="text-justify">
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
             <li> <span className="font-bold">13:30</span> - Opening Remark</li>
-            <li><span className="font-bold">13:40</span> - Keynote Talk 1 - Dr. Asim Munawar (IBM Research, US): &quot;LLM-Based Reasoning: Opportunities and Pitfalls &quot;</li>
-            <li><span className="font-bold">14:40</span> - Poster session + Coffee break</li>
-            <li><span className="font-bold">15:50</span> - Keynote Talk 2 - Dr. April Pyone Maung Maung (NII, Japan): &quot;Adversarial Attacks and Defenses on Vision-Language Models&quot;</li>
-            <li><span className="font-bold">16:40</span> - Challenge Award - Dr. Md Anwarus Salam Khan (Softbank, Japan)</li>
+            <li><span className="font-bold">13:40</span> - Keynote Talk</li>
+            <li><span className="font-bold">14:40</span> - Poster session</li>
             <li><span className="font-bold">16:55</span> - Closing Remark</li>
-            
         </ul>
     </div>
 }
