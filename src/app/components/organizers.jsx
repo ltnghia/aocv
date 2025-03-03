585646f1f03fef3f2254d3fbafb1f0ceb4a9672b
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-03 19:31:40 +0700

    update avatar

--- src/app/components/organizers.jsx
+++ src/app/components/organizers.jsx
@@ -4,14 +4,14 @@ const Organizers = ()=>{
     return <div className="text-justify"> 
          
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
-            <PersonAvatar name="Tam Nguyen" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Dayton, US"} />
+            <PersonAvatar name="Tam Nguyen" image={`${imagePrefix}/avatar.jpg`} affiliation={"University of Dayton, US"} />
             <PersonAvatar name="Minh-Triet Tran" image={`${imagePrefix}/tmtriet.png`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Trung-Nghia Le" image={`${imagePrefix}/ltnghia.jpg`} affiliation={"University of Science, Vietnam"} /> 
-            <PersonAvatar name="Khanh-Duy Le" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Science, Vietnam"} />
+            <PersonAvatar name="Khanh-Duy Le" image={`${imagePrefix}/avatar.jpg`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Duy-Nam Ly" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Viet-Tham Huynh" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Science, Vietnam"} />
-            <PersonAvatar name="Thanh-Toan Do" image={`${imagePrefix}/ldnam.jpg`} affiliation={"Monash University, Australia"} />
-            <PersonAvatar name="Vinh-Tiep Nguyen" image={`${imagePrefix}/ldnam.jpg`} affiliation={"University of Information Technology, Vietnam"} />
+            <PersonAvatar name="Viet-Tham Huynh" image={`${imagePrefix}/avatar.jpg`} affiliation={"University of Science, Vietnam"} />
+            <PersonAvatar name="Thanh-Toan Do" image={`${imagePrefix}/avatar.jpg`} affiliation={"Monash University, Australia"} />
+            <PersonAvatar name="Vinh-Tiep Nguyen" image={`${imagePrefix}/avatar.jpg`} affiliation={"University of Information Technology, Vietnam"} />
         </div>
 
         Contact: ltnghia@fit.hcmus.edu.vn
