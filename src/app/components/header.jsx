cf1746c8d2832638475081d9624aef58c5fa3fdb
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-03 13:43:17 +0700

    update header

--- src/app/components/header.jsx
+++ src/app/components/header.jsx
@@ -4,14 +4,14 @@ const Header = ()=>{
     return (
         <div className="header">
             <div className="justify-center flex flex-col sm:text-[2em] text-[1.5em] text-[#f5f5f5]  font-medium ">
-                <div className="text-white text-center text-[1.7em]">
+                {/* <div className="text-white text-center text-[1.7em]">
                      <span className="text-white tracking-[0.35em] font-semibold">AOCV </span>
-                </div>
+                </div> */}
                 <div className="text-[0.8em] text-center mt-2">
-                    ACM Multimedia 2025 Workshop on  
+                     1st Workshop on Ambiguous Object Analysis in Computer Vision (AOCV)
                 </div>
                 <div className="text-[0.8em] text-center ">
-                    Ambiguous Object Analysis in Computer Vision 
+                    ACM Multimedia 2025
                 </div>
                 <div className="text-[0.55em]  text-white text-center mt-5">
                     Dublin, Ireland 27 - 31.10.2025
