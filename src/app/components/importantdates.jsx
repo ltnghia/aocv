229342150d6a3e0fc667a3dd37bbe75fbc00cae5
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-25 16:17:18 +0700

    update date

--- src/app/components/importantdates.jsx
+++ src/app/components/importantdates.jsx
@@ -2,9 +2,9 @@ const ImportantDates = () => {
     return <div className="text-justify">
         
         <ul className="list-disc list-inside space-y-2 mt-2 ml-4">
-            <li>Paper submission deadline: <span className="font-bold"> Jul. 05, 2025 </span></li>
+            <li>Paper submission deadline: <span className="font-bold"> Jun. 13, 2025 </span></li>
             <li>Acceptance notification: <span className="font-bold"> Jul. 24, 2025 </span></li>
-            <li>Camera-ready deadline: <span className="font-bold"> Aug. 03, 2025 </span></li>
+            <li>Camera-ready deadline: <span className="font-bold"> Aug. 02, 2025 </span></li>
             <li>Workshop date: <span className="font-bold"> Otc. 27-28, 2025 </span></li>
         </ul>
     </div>
