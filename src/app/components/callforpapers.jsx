7d3a546ed8fb1f4996d089cdcf9cf0aa7fc7e4b1
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-03-28 07:00:13 +0700

    update

--- src/app/components/callforpapers.jsx
+++ src/app/components/callforpapers.jsx
@@ -12,17 +12,17 @@ const CallForPapers = () => {
             
             <p>The topics in this workshop will include but are not limited to:</p>
             <ul className="list-disc list-inside space-y-2 mt-2 mb-8 ml-4">
-                <li>Object recognition (ambiguous object such as camouflaged/transparent/in-mirror objects)</li>
-                <li>Object detection</li>
-                <li>Object segmentation</li>
-                <li>Object tracking</li>
-                <li>Object Re-ID</li>
+                <li>Ambiguous object recognition</li>
+                <li>Ambiguous object detection</li>
+                <li>Ambiguous object segmentation</li>
+                <li>Ambiguous object tracking</li>
+                <li>Ambiguous object Re-ID</li>
+                <li>Ambiguous object generation</li>
+                <li>3D reconstruction</li>
+                <li>Behavior analysis</li>
+                <li>Uncertainty analysis</li>
                 <li>Large-scale datasets</li>
                 <li>Data augmentation</li>
-                <li>3D reconstruction</li>
-                <li>Action recognition</li>
-                <li>Behavior recognition/analysis</li>
-                <li>Uncertainty analysis</li>
                 <li>Applications</li>
             </ul>
             <span className="font-bold text-[1.3em]"><a href='https://openreview.net/group?id=acmmm.org/ACMMM/2025/Workshop/AOCV' className="underline text-[1.1em] text-[#232ba1]">Submit your paper here</a></span>.
