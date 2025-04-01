e4a70529ce1ce996d24229f92ce7fdf54858c618
Trung-Nghia LE <ltnghia@users.noreply.github.com>
2025-04-01 12:52:23 +0700

    add members

--- src/app/components/organizers.jsx
+++ src/app/components/organizers.jsx
@@ -6,6 +6,7 @@ const Organizers = ()=>{
         <div className="grid sm:grid-cols-4 grid-cols-2 mt-4 gap-y-6">
             <PersonAvatar name="Tam Nguyen" image={`${imagePrefix}/nvtam.jpeg`} affiliation={"University of Dayton, US"} />
             <PersonAvatar name="Ju Shen" image={`${imagePrefix}/shenju.jpg`} affiliation={"University of Dayton, US"} />
+            <PersonAvatar name="Kunpeng Li" image={`${imagePrefix}/likunpeng.jpeg`} affiliation={"Air Force Institute of Technology, US"} />
             <PersonAvatar name="Thanh-Toan Do" image={`${imagePrefix}/dttoan.jpeg`} affiliation={"Monash University, Australia"} />
             <PersonAvatar name="Minh-Triet Tran" image={`${imagePrefix}/tmtriet.png`} affiliation={"University of Science, Vietnam"} />
             <PersonAvatar name="Trung-Nghia Le" image={`${imagePrefix}/ltnghia.jpg`} affiliation={"University of Science, Vietnam"} /> 
